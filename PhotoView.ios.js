47d610e468ba838891fbb6f98c7cc1a966918905
matthieu gayon <matthieu.gayon@gmail.com>
2017-03-13 19:47:21 +0100

    added setScale method

--- PhotoView.ios.js
+++ PhotoView.ios.js
@@ -7,16 +7,22 @@ import {
 } from 'react-native';
 
 export default class PhotoView extends Component {
+    setScale(scale) {
+        this.refs.ScrollView.setNativeProps({
+            zoomScale : scale
+        });
+    }
     render() {
         return (
             <ScrollView
+                ref='ScrollView'
                 contentContainerStyle={{ alignItems:'center', justifyContent:'center' }}
                 centerContent={true}
+                zoomScale={this.props.scale}
                 maximumZoomScale={this.props.maximumZoomScale}
-                minimumZoomScale={this.props.minimumZoomScale} 
-                showsHorizontalScrollIndicator={this.props.showsHorizontalScrollIndicator} 
-                showsVerticalScrollIndicator={this.props.showsVerticalScrollIndicator} 
-                >
+                minimumZoomScale={this.props.minimumZoomScale}
+                showsHorizontalScrollIndicator={this.props.showsHorizontalScrollIndicator}
+                showsVerticalScrollIndicator={this.props.showsVerticalScrollIndicator}>
 
                 <TouchableWithoutFeedback
                     onPress={this.props.onTap ? this.props.onTap : function() {}}>
